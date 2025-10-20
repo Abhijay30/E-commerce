deec0b224709a9b1be31b6fe693c422b2323684c
Abhijay <abhijayjr@gmail.com>
2025-10-20 14:28:00 +0530

    Initial commit

---
+++ E-commerce/src/main/java/com/jay/E_commerce/controller/ProductController.java
@@ -0,0 +1,65 @@
+package com.jay.E_commerce.controller;
+
+
+import com.jay.E_commerce.model.Product;
+import com.jay.E_commerce.service.ProductService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ProductController {
+
+    //constructor injection
+    private final ProductService Service;
+    public ProductController(ProductService service) {
+        this.Service = service;
+    }
+
+    //get all products and display them
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(Service.getAllProducts(), HttpStatus.OK);
+    }
+
+
+    //getting products by id(getting single product)
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+        Product product = Service.getProductById(id);
+
+        if(product!=null)
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+
+    //creating a new product and adding it to the database with the image
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile){
+
+        try{
+            return  new ResponseEntity<>(Service.addProduct(product, imageFile), HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NO_CONTENT);
+        }
+    }
+
+    //reading the image and displaying it
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getProductImage(@PathVariable int productId){
+        Product product = Service.getProductById(productId);
+        byte[] imageFile=product.getImageDate();
+        return ResponseEntity.ok().body(imageFile);
+    }
+}
---
+++ E-commerce/src/main/java/com/jay/E_commerce/service/ProductService.java
@@ -0,0 +1,41 @@
+package com.jay.E_commerce.service;
+
+
+import com.jay.E_commerce.model.Product;
+import com.jay.E_commerce.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    //constructor injection
+    private final ProductRepo Repo;
+    public ProductService(ProductRepo Repo) {
+        this.Repo = Repo;
+    }
+
+    //Getting all the products from repository
+    public List<Product> getAllProducts() {
+        return Repo.findAll();
+    }
+
+
+    //getting specific product from repo usind id
+    public Product getProductById(int id) {
+        return Repo.findById(id).orElse(new  Product());
+    }
+
+    //cutting down the image into name type and date so that we can store it in the database
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+
+        return Repo.save(product);
+    }
+}
